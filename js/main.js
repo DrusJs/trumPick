550a9f0c49d11a2d44eeb145c1f9d50445f441df
drusjs <magnojoky962@gmail.com>
2024-02-13 19:08:10 +0300

    update page

---
+++ js/main.js
@@ -0,0 +1,24 @@
+document.getElementById('theme-button').addEventListener('click', ()=>{
+    document.body.classList.toggle('dark-theme')
+    if (document.body.classList.contains('dark-theme')) {        
+        localStorage.setItem("isDarkTheme", true)
+    } else {
+        localStorage.setItem("isDarkTheme", '')
+    }
+})
+document.body.classList.add('transition')
+
+
+var animationContainer = document.getElementById('lottie-container');
+var animationData = {
+    container: animationContainer,
+    renderer: 'canvas', 
+    loop: true,
+    autoplay: true,
+    path: 'lottie/In_store.json'
+}
+
+var anim = lottie.loadAnimation(animationData);
+
+
+
